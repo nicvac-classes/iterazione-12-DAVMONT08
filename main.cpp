c3822192718651dd93b0f2b3b5792a7a99783709
DAVMONT08 <davide.montella@marconibari.edu.it>
2024-10-03 14:54:58 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -2,10 +2,24 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+               float Cm,Ri,i,x;
+               cout<<"inserisci il costo della moto"<<endl;
+               cin>> Cm;
+               cout<<"prima di acquistare la moto quanti risparmi hai?"<<endl;
+               cin>>Ri;
+               i=0;
+               while (Ri<Cm)
+               {
+                  cout<<"inserisci i risparmi accumulati"<<endl;
+                  cin>> x;
+                  Ri=Ri+x;
+                  cout<<"l'ammontare dei soldi accumulato è pari a " << Ri <<endl;
+                  cout<<"per comprare la moto ti servono ancora" << Cm-Ri <<endl;
+                  i=i+1;
+                 }
+            { 
+                cout<<"i risparmi sono stati effettuati " << x <<endl;
+                cout<<"dopo l'acquisto della moto ti rimangono" << Cm-Ri <<endl;
+                }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+              }
